c41249aef2948bf165266b70f997f33447bfe27d
JeanELsner <jean.elsner@googlemail.com>
2024-08-06 06:08:25 +0200

    Update install_ethercat_centos.sh

--- bin/install_ethercat_centos.sh
+++ bin/install_ethercat_centos.sh
@@ -19,37 +19,37 @@ chmod +x bootstrap
 ./configure --disable-8139too --with-linux-dir=/usr/src/linux-headers-"$(uname -r)"
 make modules
 
-sudo make modules_install install
-sudo depmod
+# sudo make modules_install install
+# sudo depmod
 
-echo "Adding ethercat group..."
-sudo groupadd ethercat
-sudo usermod -aG ethercat "$(id -un)"
+# echo "Adding ethercat group..."
+# sudo groupadd ethercat
+# sudo usermod -aG ethercat "$(id -un)"
 
-echo "Updating ethercat.conf..."
-sudo sed -i "s/\(MASTER0_DEVICE=\).*/\1\"$master0_device\"/" "/usr/local/etc/ethercat.conf"
-sudo sed -i "s/\(DEVICE_MODULES=\).*/\1\"$device_modules\"/" "/usr/local/etc/ethercat.conf"
+# echo "Updating ethercat.conf..."
+# sudo sed -i "s/\(MASTER0_DEVICE=\).*/\1\"$master0_device\"/" "/usr/local/etc/ethercat.conf"
+# sudo sed -i "s/\(DEVICE_MODULES=\).*/\1\"$device_modules\"/" "/usr/local/etc/ethercat.conf"
 
-limits_path="/etc/security/limits.conf"
-limits="
-@ethercat              soft    rtprio          99
-@ethercat              soft    priority        99
-@ethercat              soft    memlock         102400
-@ethercat              hard    rtprio          99
-@ethercat              hard    priority        99
-@ethercat              hard    memlock         102400"
+# limits_path="/etc/security/limits.conf"
+# limits="
+# @ethercat              soft    rtprio          99
+# @ethercat              soft    priority        99
+# @ethercat              soft    memlock         102400
+# @ethercat              hard    rtprio          99
+# @ethercat              hard    priority        99
+# @ethercat              hard    memlock         102400"
 
-echo "Adding ethercat group to limits.conf..."
-if grep -qF "@ethercat" "$limits_path"; then
-    echo "limits.conf already contains ethercat group."
-else
-    echo "$limits" | sudo tee -a "$limits_path" >/dev/null
-fi
+# echo "Adding ethercat group to limits.conf..."
+# if grep -qF "@ethercat" "$limits_path"; then
+#     echo "limits.conf already contains ethercat group."
+# else
+#     echo "$limits" | sudo tee -a "$limits_path" >/dev/null
+# fi
 
-echo "Setting device access permissions..."
-sudo sh -c 'echo KERNEL==\"EtherCAT[0-9]*\", MODE=\"0660\", GROUP=\"ethercat\" > /lib/udev/rules.d/99-EtherCAT.rules'
-echo "Registering libraries with dynamic linker..."
-sudo sh -c 'echo /usr/local/lib > /etc/ld.so.conf.d/etherlab.conf'
-sudo ldconfig
+# echo "Setting device access permissions..."
+# sudo sh -c 'echo KERNEL==\"EtherCAT[0-9]*\", MODE=\"0660\", GROUP=\"ethercat\" > /lib/udev/rules.d/99-EtherCAT.rules'
+# echo "Registering libraries with dynamic linker..."
+# sudo sh -c 'echo /usr/local/lib > /etc/ld.so.conf.d/etherlab.conf'
+# sudo ldconfig
 
-echo "Done."
+# echo "Done."
